c4dbf96b740d070d1e68021b59f882739b7b259a
Felipe Thiago <78160698+Felipe-Thiago@users.noreply.github.com>
2026-03-13 21:17:00 -0300

    Create dados_compra table for purchase data

--- banco.sql
+++ banco.sql
@@ -1,8 +1,10 @@
-CREATE TABLE dados (
-    AlunoID int,
-    Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
+CREATE TABLE dados_compra (
+    CompraID int not null,
+    Nome_Cliente varchar(50) not null,
+    CPF varchar(14),
     Cidade varchar(50),
+    Valor decimal(10, 2) not null,
+    OpcaoPagamento varchar(50),
+    Data_Compra DATETIME DEFAULT CURRENT_TIMESTAMP,
     Host varchar(50)
 );
